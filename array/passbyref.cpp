e2cef3a5ffe38b75ee1c5658aa3335e79084bc7b
Sameer Kulkarni <samgrk14@gmail.com>
2026-03-20 13:28:23 +0530

    click for code

--- array/passbyref.cpp
+++ array/passbyref.cpp
@@ -15,3 +15,16 @@ int main(){
     }
     return 0;
 }
+
+// array is the non premitive data type in c++ and it is used to store multiple values of the same type in a single variable.
+
+
+//  pass by refernece in array is possible because when we pass an array
+
+// to a function, we are actually passing a pointer to the first element of the array.
+
+// This means that any changes made to the array within the function will affect the original array outside the function.
+
+// In the example above, the changeArr function modifies the elements of the array by doubling their values,
+
+// and these changes are reflected in the main function when we print the array after calling changeArr.
